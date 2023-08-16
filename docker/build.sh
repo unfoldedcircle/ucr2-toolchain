e494c1c4c212c649aedd39f13f3f7f44b4fcc68d
Markus Zehnder <business@markuszehnder.ch>
2023-08-16 14:39:48 +0200

    build: update Docker build script for SDK 1.0.0

--- docker/build.sh
+++ docker/build.sh
@@ -3,7 +3,7 @@
 set -o errexit
 set -o pipefail
 
-SDK_VERSION=0.6.0
+SDK_VERSION=1.0.0
 QT_VERSION_MINOR=5.15
 QT_VERSION_PATCH=8
 QT_VERSION=$QT_VERSION_MINOR.$QT_VERSION_PATCH
@@ -15,7 +15,10 @@ VERSION_ARGS="\
 
 # use a local webserver to speed up archive downloads
 # Qt mirrors: https://download.qt.io/archive/qt/5.15/5.15.8/single/qt-everywhere-opensource-src-5.15.8.tar.xz.mirrorlist
-# QT_MIRROR URL must contain base base **before** `/archive/qt/`
+# QT_MIRROR URL must contain base path **before** `/archive/qt/`
+# Local file server example for SDK_VERSION=1.0.0, QT_VERSION_MINOR=5.15, QT_VERSION_PATCH=8 with files located at:
+# - toolchain: /archive/ucr2/v1.0.0/ucr2-aarch64-toolchain-1.0.0-noqt.tar.gz
+# - qt:        /qt/5.15/5.15.8/single/qt-everywhere-opensource-src-5.15.8.tar.xz
 #MIRROR_ARGS="\
 #--build-arg SDK_BASE_URL=http://172.16.16.10/archive/ucr2 \
 #--build-arg QT_MIRROR=http://172.16.16.10"
